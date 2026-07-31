6de0a80491cd3a22cf533ce6f94e9d64b7ef4236
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-07-31 18:51:40 +0530

    Time: 15 ms (35.66%) | Memory: 25.4 MB (84.45%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,38 +5,27 @@ public:
     // }
     int minimumPushes(string word) {
         int n=word.size();
-        int freq[26]={0};
+        vector<int> freq(26,0);
 
         for(int i=0;i<n;i++){
             freq[word[i]-'a']++;
         }
-
-        vector<pair<char,int>> v;
-
-        for(int i=0;i<26;i++){
-            v.push_back({'a'+i,freq[i]});
-        }
-
-        sort(v.begin(), v.end(),
-         [](const auto &a, const auto &b) {
-             return a.second > b.second;
-         });
+        sort(freq.begin(),freq.end(),[](int a,int b){
+            return a>b;
+        });
 
         int ans=0;
         int count=0;
         int key=1;
         for(int i=0;i<26;i++){
-            char ch=v[i].first;
-            int fre=v[i].second;
-            if(fre==0) continue;
-            cout<<"character: "<<ch<<"->"<<"freq: "<<fre<<endl;
-            ans+=key*fre;
+            if(freq[i]==0) break;
+            ans+=freq[i]*key;
             count++;
-
             if(count==8){
-                key++;
                 count=0;
+                key++;
             }
+            
         }
         return ans;
     }
