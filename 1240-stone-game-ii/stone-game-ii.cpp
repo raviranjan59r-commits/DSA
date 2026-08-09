f4deb284065e403253dd1975f0d7043cf36aa6f9
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-10 01:44:08 +0530

    Time: 15 ms (35.67%) | Memory: 16.5 MB (25.66%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int helper(vector<int> & piles,int i,int m){
+        if(i>=piles.size()) return 0;
+        if(dp[i][m]!=INT_MIN) return dp[i][m];
+        int sum=0;
+        int ans=INT_MIN;
+        for(int t=i;t<(i+2*m);t++){
+            if(t>=piles.size()) break;
+            sum+=piles[t];
+            ans=max(ans,sum-helper(piles,t+1,max(m,t-i+1)));
+        }
+        return dp[i][m]=ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+        dp.resize(100,vector<int>(100,INT_MIN));
+        int total = accumulate(piles.begin(), piles.end(), 0);
+        int diff = helper(piles, 0, 1);
+        return (total + diff) / 2;
+    }
+};
