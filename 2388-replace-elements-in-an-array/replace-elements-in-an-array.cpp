900375521439a2cec74a57abc3f6ccb34b1825ad
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-15 16:43:27 +0530

    Time: 106 ms (53.64%) | Memory: 134.2 MB (43.32%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        unordered_map<int,int> mp;// it will stroe ele:index
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i;
+        }
+
+        for(int i=0;i<operations.size();i++){
+            int idx=mp[operations[i][0]];
+            mp.erase(operations[i][0]);
+            mp[operations[i][1]]=idx;
+        }
+        
+        for(auto ele: mp){
+            nums[ele.second]=ele.first;
+        }
+
+        return nums;
+    }
+};
