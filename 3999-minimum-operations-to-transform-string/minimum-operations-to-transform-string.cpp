90efb229816998f2e57577677cdf86674a2934e7
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-15 22:06:06 +0530

    Time: 29 ms (41.88%) | Memory: 59.9 MB (57.79%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -6,25 +6,15 @@ public:
         for(int i=0;i<s.length();i++){
             freq[s[i]-'a']++;
         }
-        // int count=0;
-        vector<char> abc;
+        int count=0;
+        // vector<char> abc;
 
         for(int i=1;i<26;i++){
             if(freq[i]!=0){
-                abc.push_back('a'+i);
+               char ch='a'+i;
+               return 26 - (ch - 'a');
             } 
         }
-        abc.push_back('a');
-
-        for(auto ele:abc) cout<<ele<<" ";
-
-        int count=0;
-        for(int i=0;i<abc.size()-1;i++){
-            count+= (26-(abc[i]-abc[i+1]))%26;
-        }
-
-        // ans+=26-(s[i]-'a');
-
         return count;
 
     }
