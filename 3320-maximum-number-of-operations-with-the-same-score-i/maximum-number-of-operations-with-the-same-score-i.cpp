feceb29ecdf19d55b7d1a9a6e35d288c353954de
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-16 16:37:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (94.22%) - LeetSync

---
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums) {
+        int n=nums.size();
+        int sum=nums[0]+nums[1];
+        int count=1;
+        for(int i=2;i<n-1;i+=2){
+            if(sum==(nums[i]+nums[i+1])){
+                count++;
+            }
+            else break;
+        }
+        return count;
+    }
+};
