076ba18c7b9aa3357bcdc6b6801e854e0aaf7bf0
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-17 00:20:49 +0530

    Time: 5 ms (48.73%) | Memory: 131.2 MB (69.29%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+
+        for(int i=0;i<stones.size();i++){
+            if(stones[i]%3==0) count0++;
+            else if(stones[i]%3==1) count1++;
+            else count2++;
+        }
+
+        if(count0%2==0){
+            return count1>0 && count2>0;
+        }
+
+        return abs(count1-count2)>2;
+    }
+};
