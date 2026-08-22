c71092711b0496d207e8d70e4cbc8e1cd34a2213
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-22 13:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.81%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+
+        int l=0;
+        int r=n-1;
+
+        while(l<=r){
+            int mid=r+(l-r)/2;
+
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid-1]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
