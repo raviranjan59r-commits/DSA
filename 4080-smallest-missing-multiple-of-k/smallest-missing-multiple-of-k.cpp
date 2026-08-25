303aceb213554ce54adae465e6c3f775d223ccfc
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-25 13:18:23 +0530

    Time: 1 ms (47.29%) | Memory: 25 MB (45.06%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -3,7 +3,7 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         unordered_set<int> s(nums.begin(),nums.end());
 
-        for(int i=k;i<=k*(nums.size()+1);i+=k){
+        for(int i=k;;i+=k){
             if(!s.count(i)) return i;
         }
         return -1;
