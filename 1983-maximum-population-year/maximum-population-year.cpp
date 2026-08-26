f177a7b5e916ff08aa2c51338b851f9aa7cd9c6f
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-26 15:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (79.15%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        //by using line sweep technique
-        map<int,int> events;
+        //using difference array
+        int year[102]={0};
 
         for(int i=0;i<logs.size();i++){
-            events[logs[i][0]]++;
-            events[logs[i][1]]--;
+            year[logs[i][0] - 1950]+=1;
+            year[logs[i][1] - 1950]-=1;
         }
         int maxpopulation=0;
         int population=0;
-        int ans;
-        for(auto &[year,delta]:events){
-            population+=delta;
-
+        int ans=1950;
+        for(int i=0;i<102;i++){
+            population+=year[i];
             if(population>maxpopulation){
                 maxpopulation=population;
-                ans=year;
+                ans=1950+i;
             }
         }
         return ans;
