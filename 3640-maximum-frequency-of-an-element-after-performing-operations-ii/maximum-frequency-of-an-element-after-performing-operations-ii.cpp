4e9fa3b5e69cfbb5ae1e6992f92c12f9da8f75b9
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-29 16:59:14 +0530

    Time: 1370 ms (30.34%) | Memory: 291.4 MB (28.16%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        map<int,int> events;
+
+        unordered_map<int,int> freq;
+        for(int ele:nums){
+            freq[ele]++;// calculate the freq
+            events[ele]=0;// add this current ele to the event
+        }
+
+
+        for(int ele:nums){
+            int l=ele-k;
+            int r=ele+k;
+            events[l]++;
+            events[r+1]--;
+        }
+        //prefix sum of freq
+        int freqency=0;
+        for(auto &it:events){
+            it.second+=freqency;
+            freqency=it.second;
+        }
+        //now all the elements are present in sorted order and with their freq in it
+        // important observation -> events will alaways contain the ans element as key in it
+        int max_freq=0;
+        for(auto & [ele,f]:events){
+            int current_max_freq=min(numOperations,f-freq[ele]) + freq[ele];
+            max_freq=max(max_freq,current_max_freq);
+        }
+        return max_freq;
+    }
+};
