766d69653dc2369f95b21e5e36e8551a97a7e39a
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-08-29 17:05:10 +0530

    Time: 774 ms (9.10%) | Memory: 213 MB (13.50%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int n=nums.size();
-        int mx=*max_element(nums.begin(),nums.end());
-        vector<int> diff(mx+2,0);
-        unordered_map<int,int> mp;
-        for(auto & ele:nums){
-            int l=max(0,ele-k);
-            int r=min(mx,ele+k);
-            mp[ele]++;
 
-            diff[l]++;
-            diff[r+1]--;
-        }
-        //prefix sum
+        //method 2
+        map<int,int> events;
 
-        for(int i=1;i<diff.size();i++){
-            diff[i]+=diff[i-1];
+        unordered_map<int,int> freq;
+        for(int ele:nums){
+            freq[ele]++;// calculate the freq
+            events[ele]=0;// add this current ele to the event
         }
-       int max_freq=0;
-        for(int i=0;i<=mx;i++){
-            int current_ele_max_freq=min(diff[i]-mp[i],numOperations)   + mp[i];
-            max_freq=max(max_freq,current_ele_max_freq);
+
+
+        for(int ele:nums){
+            int l=ele-k;
+            int r=ele+k;
+            events[l]++;
+            events[r+1]--;
+        }
+        //prefix sum of freq
+        int freqency=0;
+        for(auto &it:events){
+            it.second+=freqency;
+            freqency=it.second;
+        }
+        //now all the elements are present in sorted order and with their freq in it
+        // important observation -> events will alaways contain the ans element as key in it
+        int max_freq=0;
+        for(auto & [ele,f]:events){
+            int current_max_freq=min(numOperations,f-freq[ele]) + freq[ele];
+            max_freq=max(max_freq,current_max_freq);
         }
         return max_freq;
-
     }
 };
