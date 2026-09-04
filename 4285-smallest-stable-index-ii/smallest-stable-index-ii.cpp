a73079a0441a551485c615c33c3c405217ec184e
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-04 12:04:03 +0530

    Time: 11 ms (58.02%) | Memory: 211 MB (15.33%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        // minor optimisation
+
         int n=nums.size();
-
-        // calculate prefix maximum
-        vector<int> prefix_maximum(n,-1);
-        prefix_maximum[0]=nums[0];
-
-        for(int i=1;i<n;i++){
-            prefix_maximum[i]=max(prefix_maximum[i-1],nums[i]);
-        }
-
         // suffix minimum
         vector<int> suffix_minimum(n,-1);
         suffix_minimum[n-1]=nums[n-1];
@@ -19,13 +12,21 @@ public:
             suffix_minimum[i]=min(suffix_minimum[i+1],nums[i]);
         }
 
-        // calculate ans
+        // calculate prefix maximum and calculate ans
+        vector<int> prefix_maximum(n,-1);
+        prefix_maximum[0]=nums[0];
+        if(prefix_maximum[0]-suffix_minimum[0] <= k ){
+            return 0;
+        }
         int idx=-1;
-        for(int i=0;i<n;i++){
+
+        for(int i=1;i<n;i++){
+            prefix_maximum[i]=max(prefix_maximum[i-1],nums[i]);
             if(prefix_maximum[i]-suffix_minimum[i] <= k ){
                 return i;
             }
         }
+
         return idx;
     }
 };
