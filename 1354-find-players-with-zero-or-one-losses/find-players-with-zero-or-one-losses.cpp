7200df5c1d91c866a043d989996e2f0203ee1c7d
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-08 01:30:11 +0530

    Time: 274 ms (23.37%) | Memory: 177.6 MB (39.33%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int> mp; // store no of loses
+        map<int,int> win;
+        map<int,int> loss;
 
         for(auto &v:matches){
-            mp[v[1]]++;
+            win[v[0]]++;
+            loss[v[1]]++;
         }
 
         vector<int> not_lost;
         vector<int> one_lost;
 
-        for(auto &v:matches){
-            if(!mp.count(v[0])) not_lost.push_back(v[0]);
-            if(!mp.count(v[1])) not_lost.push_back(v[0]);
+        for(auto &[player,lose]:loss){
+            if(lose==1) one_lost.push_back(player);
         }
-
-        for(auto &[player,loss]:mp){
-            if(loss==1) one_lost.push_back(player);
+        for(auto &[player,w]:win){
+            if(!loss.count(player)) not_lost.push_back(player);
         }
-        sort(not_lost.begin(),not_lost.end());
-        not_lost.erase(unique(not_lost.begin(),not_lost.end()),not_lost.end());//remove duplicate elemrnts
+       
 
         return {not_lost,one_lost};
     }
