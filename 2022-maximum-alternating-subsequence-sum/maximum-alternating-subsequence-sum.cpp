754c9e560b69a8143efe0e1b8da8f82e053b5793
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-20 02:41:42 +0530

    Time: 224 ms (58.84%) | Memory: 157.2 MB (45.12%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -5,6 +5,7 @@ public:
         if(i>=nums.size()) return 0;
         //not take
         if(dp[i][even]!=-1) return dp[i][even];
+
         long long not_take=solve(nums,i+1,even);
         long long take;
         if(even){
@@ -19,7 +20,20 @@ public:
     }
     long long maxAlternatingSum(vector<int>& nums) {
         long long n=nums.size();
-        dp.resize(n,vector<long long>(2,-1));
-        return solve(nums,0,true);
+        dp.resize(n+1,vector<long long>(2,0));
+
+        for(int i=n-1;i>=0;i--){
+            // even=true
+
+            dp[i][1]=max(dp[i+1][1],nums[i]+dp[i+1][0]);
+
+
+            // even=false
+
+            dp[i][0]=max(dp[i+1][0],dp[i+1][1]-nums[i]);
+
+            
+        }
+        return dp[0][1];
     }
 };
