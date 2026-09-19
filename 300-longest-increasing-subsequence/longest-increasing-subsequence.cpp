183e643c8d4ca78c40e68222b8bb59852f249baa
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-20 03:10:05 +0530

    Time: 183 ms (36.53%) | Memory: 41.6 MB (35.26%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -17,7 +17,22 @@ public:
         return dp[i][prevIdx+1] = max(skip,take);
     }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return LIS(nums,0,-1);
+        memset(dp,0,sizeof(dp));
+        // return LIS(nums,0,-1);
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            for(int prevIdx=i-1;prevIdx>=-1;prevIdx--){
+                int skip=dp[i+1][prevIdx+1];
+
+                int take=-1e9;
+
+                if(prevIdx==-1 || nums[i]>nums[prevIdx]){
+                    take=1+dp[i+1][i+1];
+                }
+
+                dp[i][prevIdx+1]=max(take,skip);
+            }
+        }
+        return dp[0][0];
     }
 };
