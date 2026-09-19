30c5726e5cb7f15eb8979b3823b03970553600d2
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-20 02:42:48 +0530

    Time: 266 ms (43.77%) | Memory: 157 MB (56.92%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
-    vector<vector<long long>> dp;
-    long long solve(vector<int>&nums,long long i,bool even){
-        if(i>=nums.size()) return 0;
-        //not take
-        if(dp[i][even]!=-1) return dp[i][even];
-
-        long long not_take=solve(nums,i+1,even);
-        long long take;
-        if(even){
-            take=solve(nums,i+1,!even) + nums[i];
-        }
-        else{
-            take=solve(nums,i+1,!even) - nums[i];
-        }
-
-        return dp[i][even]=max(take,not_take);
-
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-        long long n=nums.size();
-        dp.resize(n+1,vector<long long>(2,0));
+        int n=nums.size();
+        vector<vector<long long>>dp(n+1,vector<long long>(2,0));
 
         for(int i=n-1;i>=0;i--){
             // even=true
