9a7bd6da31d3f8ff3fd83da5b31dcf72ce9b67ba
raviranjan59r-commits <raviranjan59r@gmail.com>
2026-09-20 15:39:53 +0530

    Time: 123 ms (64.98%) | Memory: 10 MB (50.51%) - LeetSync

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long t[51][101][51];
+    int M=1e9+7;
+    int solve(int size,int lastMax,int score,int n,int m,int k){
+        if(size>n) return 0;
+        if(size==n ){
+            return score==k;
+        }
+        if(score>k) return 0;
+        if(lastMax!=-1 && t[size][lastMax][score]!=-1) return t[size][lastMax][score];
+        // choosing between 1 to m
+        long long res=0;
+        for(int i=1;i<=m;i++){
+            long long take;
+            if(i>lastMax){
+                take=solve(size+1,i,score+1,n,m,k);
+            }
+            else{
+                take=solve(size+1,lastMax,score,n,m,k);
+            }
+
+            res=(res+take)%M;
+        }
+        if(lastMax!=-1){
+            t[size][lastMax][score]=res;
+        }
+        return res;
+
+    }
+    int numOfArrays(int n, int m, int k) {
+        memset(t,-1,sizeof(t));
+        return solve(0,-1,0,n,m,k);
+    }
+};
